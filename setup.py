f770341b5b649067fd287db739519a863e36cf56
sallfarr77 <sallfarr@outlook.com>
2025-01-06 13:49:05 +0700

    Fix setup.py metadata

--- setup.py
+++ setup.py
@@ -3,18 +3,29 @@ from setuptools import setup, find_packages
 setup(
     name="ossh",
     version="1.0.0",
-    description="A Python tool for SSH-related utilities.",
+    description="Professional SSH Connection Manager",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Salman Al Farisi",
     author_email="sallman.alffarisi@gmail.com",
     url="https://github.com/sallfarr77/ossh",
+    packages=find_packages(),
     py_modules=["ossh"],
-    install_requires=open("requirements.txt").read().splitlines(),
+    install_requires=[
+        "rich>=13.9.4",
+    ],
     entry_points={
         "console_scripts": [
-            "ossh=ossh:main",  
+            "ossh=ossh:main",
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
     license="MIT",
+    include_package_data=True,
 )
